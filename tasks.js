7d8f23af5d004daf91bb51b35131ad750e73adc0
joaofelima96 <jf-lima1996@hotmail.com>
2023-04-12 19:41:35 -0300

    JS - Criando a lista de dados

---
+++ tasks.js
@@ -0,0 +1,12 @@
+let taskData = [
+    {
+        id: uid(),
+        name: 'Ver se eu to na esquina',
+        toDo: true,
+    },
+    {
+        id: uid(),
+        name: 'Terminar as aulas de FrontEnd',
+        toDo: false,
+    }
+]
